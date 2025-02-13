07ef17f7b58536299db3e8a35cebb3495bbfbc25
Erfan_Fazel <erfanfazel1389@gmail.com>
2025-02-13 09:52:28 +0330

    update OrgenizeFiles to get directory in obj __call__

--- src/clean.py
+++ src/clean.py
@@ -3,28 +3,29 @@ from pathlib import Path
 from src.data import DATA_DIR
 from src.utils.io import read_json
 from loguru import logger
+from typing import Union
 class OrgenizeFiles:
     """
     This class is used to orgenize files in a directory by
     moving files into directories based on extension
     """
-    def __init__(self,directory):
-        self.directory = Path(directory)
-        if not self.directory.exists():
-            raise FileNotFoundError(f"{self.directory} does not exist")
+    def __init__(self):
         ext_dirs = read_json(DATA_DIR / "extensions.json")
         self.extentions_dest = {}
         for dir_name,ext_list in ext_dirs.items():
             for ext in ext_list:
                 self.extentions_dest[ext] = dir_name
-    def __call__(self):
+    def __call__(self,directory:Union[str,Path]):
         """
         Orgenize files in the directory by moving them to 
         sub directories based on extension
         """
-        logger.info(f"Orgenizing files in {self.directory}...")
+        directory = Path(directory)
+        if not directory.exists():
+            raise FileNotFoundError(f"{self.directory} does not exist")
+        logger.info(f"Orgenizing files in {directory}...")
         file_extensions = []
-        for file_path in self.directory.iterdir():
+        for file_path in directory.iterdir():
             #ignored directories
             if file_path.is_dir():
                 continue
@@ -41,5 +42,5 @@ class OrgenizeFiles:
             shutil.move(str(file_path),DEST_DIR)
 
 if __name__ == "__main__":
-    org_file = OrgenizeFiles(directory="/mnt/c/Users/Erfan/Downloads/Telegram Desktop")
-    org_file()
+    org_file = OrgenizeFiles()
+    org_file(directory="/mnt/c/Users/Erfan/Downloads/Telegram Desktop")
